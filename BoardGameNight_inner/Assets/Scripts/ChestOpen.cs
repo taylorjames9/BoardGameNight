d2ae802237f0966f8bf77f05f717303b8b0f6e91
ci3-122-mac2-taylorjames9 <taylorjames9@gmail.com>
2014-10-29 18:18:05 -0500

    Added an open chest

--- BoardGameNight_inner/Assets/Scripts/ChestOpen.cs
+++ BoardGameNight_inner/Assets/Scripts/ChestOpen.cs
@@ -5,13 +5,16 @@ public class ChestOpen : MonoBehaviour {
 
 
 	int hitCounter = 0;
+	public Sprite openChest;
+	public Sprite closedChest;
 
 	public GameObject myFireExtinguisher;
 	//ArrayList<GameObject> chestItems = new ArrayList<GameObject> ();
 	GameObject[] chestItems;
 
 	void Start(){
-		//chestItems.add (myFireExtinguisher);
+		//renderer.material.mainTexture = closedChest;
+		gameObject.GetComponent<SpriteRenderer>().sprite = closedChest;
 		myFireExtinguisher.SetActive (false);
 	}
 
@@ -20,7 +23,8 @@ public class ChestOpen : MonoBehaviour {
 		if (coll.gameObject.tag == "Main") {
 			hitCounter++;
 			myFireExtinguisher.SetActive (true);
-
+			gameObject.GetComponent<SpriteRenderer> ().sprite = openChest;
+			//renderer.material.mainTexture = openChest;
 		}
 	}
 }
